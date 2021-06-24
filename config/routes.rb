1388c8c8f20170808e3f0319f606345e2ac63b22
ryunosuke35 <6bev2103@gmail.com>
2021-06-24 14:31:00 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :blogs
-  resources :users
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
