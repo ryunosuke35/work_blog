94cbb6a661918b16e89d0b3795cde59a20d2bd50
ryunosuke35 <6bev2103@gmail.com>
2021-06-24 14:56:00 +0900

    新規登録と同時にログインをするよう実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ skip_before_action :login_required, only: [:new, :create]
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render :new
